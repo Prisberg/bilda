a1793999e560fef445f4bbe48c3eadffbaec0774
Prisberg <fille.risberg@live.se>
2022-07-14 17:30:43 +0200

    errorboundary

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ErrorBoundary from "./ErrorBoundary";
 import StartPage from './StartPage';
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="" element={<StartPage/>} />
-        {/* <Route path="" element={ } />
+      <ErrorBoundary>
+        <Routes>
+          <Route path="" element={<StartPage />} />
+          {/* <Route path="" element={ } />
         <Route path="" element={ } /> */}
-      </Routes>
+        </Routes>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 }
