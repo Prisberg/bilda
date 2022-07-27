3b7fbf47d9f84c0679251e8590e96b36554c5133
Prisberg <fille.risberg@live.se>
2022-07-27 21:57:38 +0200

    small stuff

--- src/components/HandlePersonalQuestions.tsx
+++ src/components/HandlePersonalQuestions.tsx
@@ -1,18 +1,16 @@
-import * as React from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+import { Box, SxProps } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
-import ListItemText from '@mui/material/ListItemText';
-import ListItem from '@mui/material/ListItem';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import CloseIcon from '@mui/icons-material/Close';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 import Slide from '@mui/material/Slide';
+import Toolbar from '@mui/material/Toolbar';
 import { TransitionProps } from '@mui/material/transitions';
-import { Box, SxProps, TextField } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
 
 const Transition = React.forwardRef(function Transition(
     props: TransitionProps & {
--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import CreateQuestion from "./CreateQuestion";
 import HandlePersonalQuestions from "./HandlePersonalQuestions";
 
